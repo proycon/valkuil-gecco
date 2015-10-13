29f5760ca1ae53ee4b6b3d88e0741084c2c1db0e
Maarten van Gompel <proycon@anaproy.nl>
2015-10-13 12:22:06 +0200

    fix

--- download-models.sh
+++ download-models.sh
@@ -7,7 +7,7 @@ fi
 
 echo "Downloading big data"
 PREDOWNLOAD=`stat --printf='%X' valkuil-gecco-data.tar.bz2 2> /dev/null`
-wget -c -N http://lst.science.ru.nl/~proycon/valkuil-gecco-data.tar.gz2
+wget -c -N http://lst.science.ru.nl/~proycon/valkuil-gecco-data.tar.bz2
 POSTDOWNLOAD=`stat --printf='%X' valkuil-gecco-data.tar.bz2 2> /dev/null`
 if [ "$PREDOWNLOAD" != "$POSTDOWNLOAD" ]; then
     tar -xvjf valkuil-gecco-data.tar.bz2
